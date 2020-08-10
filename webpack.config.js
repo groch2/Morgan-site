2f71100d54783b2f51cbd5613d12efad0229508c
Roch DESCHASEAUX <roch.deschaseaux@mafassurances.onmicrosoft.com>
2020-08-10 17:12:05 +0200

    add webpack bundle-ization

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './index.html',
+    output: {
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [{
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [{
+                    loader: 'file-loader',
+                }, ],
+            },
+        ],
+    },
+};
