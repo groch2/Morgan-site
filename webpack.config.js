97ef66d4b8e94477816c8c1fb22d7aa594f4dae6
Roch DESCHASEAUX <roch.deschaseaux@mafassurances.onmicrosoft.com>
2020-08-11 13:35:04 +0200

    use js file as webpack entry

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path');
 
 module.exports = {
     mode: 'development',
-    entry: './index.html',
+    entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist')
     },
