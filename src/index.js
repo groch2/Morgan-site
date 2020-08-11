74ab2c754c8158fb3319ea0cc42bfa9ece01f7da
Roch DESCHASEAUX <roch.deschaseaux@mafassurances.onmicrosoft.com>
2020-08-11 13:52:48 +0200

    load w3 css from node modules

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import '../css/styles.css';
+// import picture from '../1 Peintures/Nordique, ht, 33x41cm, 2019.jpg';
+
+// const homePicture = new Image();
+// homePicture.src = picture;
+// homePicture.className = "w3-image";
+// document.body.children[0].appendChild(homePicture);
