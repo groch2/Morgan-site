6bef6d898e5ea1755651673705e7dbdaf605c9a3
Roch DESCHASEAUX <roch.deschaseaux@mafassurances.onmicrosoft.com>
2020-10-09 19:41:02 +0200

    use strict mode for webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+"use strict"
+
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin');
