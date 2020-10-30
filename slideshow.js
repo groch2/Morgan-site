ca310a921655eeae72bff2bc47e3a3e077f9ea0e
Roch DESCHASEAUX <roch.deschaseaux@mafassurances.onmicrosoft.com>
2020-10-30 15:59:54 +0100

    Fix bug in slideshow navigation

--- slideshow.js
+++ slideshow.js
@@ -10,7 +10,7 @@ function closeModal() {
 
 const pictures = document.getElementsByClassName("picture");
 const pictureLabels = document.getElementsByClassName("picture-label");
-var slideIndex = 1;
+let slideIndex = 0;
 showDivs(slideIndex);
 
 function plusDivs(n) {
@@ -18,22 +18,22 @@ function plusDivs(n) {
 }
 
 function showDivs(n) {
-    if (n > pictures.length) {
-        slideIndex = 1;
+    if (n === pictures.length) {
+        slideIndex = 0;
     }
-    if (n < 1) {
-        slideIndex = pictures.length;
+    if (n === -1) {
+        slideIndex = pictures.length - 1;
     }
-    for (var i = 0; i < pictures.length; i++) {
+    for (let i = 0; i < pictures.length; i++) {
         [pictures, pictureLabels]
             .forEach(
                 htmlElements =>
-                    htmlElements[i].style.display = i === slideIndex - 1 ? "block" : "none");
+                    htmlElements[i].style.display = i === slideIndex ? "block" : "none");
     }
 }
 
 document.getElementById("openBtn").addEventListener("click", openModal);
 document.getElementById("closeBtn").addEventListener("click", closeModal);
 
-document.getElementById("plusBtn").addEventListener("click", function () { plusDivs(+1); });
+document.getElementById("plusBtn").addEventListener("click", function () { plusDivs(1); });
 document.getElementById("minusBtn").addEventListener("click", function () { plusDivs(-1); });
