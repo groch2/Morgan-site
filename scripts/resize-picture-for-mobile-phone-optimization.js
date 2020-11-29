13b2789117c3cf1f9d57d808ed19eedf6533af83
Roch DESCHASEAUX <roch.deschaseaux@mafassurances.onmicrosoft.com>
2020-11-29 21:23:06 +0100

    Get dimensions of each image

--- scripts/resize-picture-for-mobile-phone-optimization.js
+++ scripts/resize-picture-for-mobile-phone-optimization.js
@@ -2,6 +2,7 @@
 
 const fs = require("fs");
 const path = require("path");
+const sizeOf = require("image-size");
 const sharp = require("sharp");
 
 const picturesDirectory = "./pictures";
@@ -9,9 +10,13 @@ const pictures = fs
   .readdirSync(picturesDirectory, { withFileTypes: true })
   .filter((dirent) => dirent.isDirectory())
   .flatMap(({ name: directory }) =>
-    fs
-      .readdirSync(path.join(picturesDirectory, directory))
-      .map((picture) => path.join(directory, picture))
+    fs.readdirSync(path.join(picturesDirectory, directory)).map((picture) => {
+      const pictureRef = path.join(directory, picture);
+      const { height, width } = sizeOf(
+        path.join(picturesDirectory, pictureRef)
+      );
+      return { pictureRef, height, width };
+    })
   );
 
 console.debug({ pictures });
