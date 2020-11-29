a279f039c3dc06dcff6966d103501b39440ef16a
Roch DESCHASEAUX <roch.deschaseaux@mafassurances.onmicrosoft.com>
2020-11-29 22:38:42 +0100

    Write resized and converted versions of pictures for mobile phone display.

--- scripts/resize-picture-for-mobile-phone-optimization.js
+++ scripts/resize-picture-for-mobile-phone-optimization.js
@@ -9,29 +9,45 @@ const picturesDirectory = "./pictures";
 const iPhone12Height = 1284;
 const iPhone12Width = 2778;
 const iPhoneRatio = iPhone12Height / iPhone12Width;
-const pictures = fs
+const pictureDirectories = fs
   .readdirSync(picturesDirectory, { withFileTypes: true })
-  .filter((dirent) => dirent.isDirectory())
-  .flatMap(({ name: directory }) =>
-    fs.readdirSync(path.join(picturesDirectory, directory)).map((picture) => {
-      const pictureRef = path.join(directory, picture);
-      const { height: pictureHeight, width: pictureWidth } = sizeOf(
-        path.join(picturesDirectory, pictureRef)
-      );
-      const pictureRatio = pictureHeight / pictureWidth;
-      const isPhoneMoreStretchedThanPicture = iPhoneRatio < pictureRatio;
-      const pictureHeightOnIPhone = isPhoneMoreStretchedThanPicture
-        ? iPhone12Height
-        : (pictureHeight * iPhone12Width) / pictureWidth;
-      const pictureWidthOnIPhone = isPhoneMoreStretchedThanPicture
-        ? (pictureWidth * iPhone12Height) / pictureHeight
-        : iPhone12Width;
-      return {
-        pictureRef,
-        height: Math.round(pictureHeightOnIPhone),
-        width: Math.round(pictureWidthOnIPhone),
-      };
-    })
-  );
-
-console.debug({ pictures });
+  .filter((dirent) => dirent.isDirectory());
+const pictures = pictureDirectories.flatMap(({ name: directory }) =>
+  fs.readdirSync(path.join(picturesDirectory, directory)).map((picture) => {
+    const ref = path.join(directory, picture);
+    const { height: pictureHeight, width: pictureWidth } = sizeOf(
+      path.join(picturesDirectory, ref)
+    );
+    const pictureRatio = pictureHeight / pictureWidth;
+    const isPhoneMoreStretchedThanPicture = iPhoneRatio < pictureRatio;
+    const pictureHeightOnIPhone = isPhoneMoreStretchedThanPicture
+      ? iPhone12Height
+      : (pictureHeight * iPhone12Width) / pictureWidth;
+    const pictureWidthOnIPhone = isPhoneMoreStretchedThanPicture
+      ? (pictureWidth * iPhone12Height) / pictureHeight
+      : iPhone12Width;
+    return {
+      ref,
+      height: Math.round(pictureHeightOnIPhone),
+      width: Math.round(pictureWidthOnIPhone),
+    };
+  })
+);
+const mobilePicturesDirectory = "./mobilePictures";
+fs.rmdirSync(mobilePicturesDirectory, { recursive: true });
+fs.mkdirSync(mobilePicturesDirectory);
+pictureDirectories.forEach(({ name: directory }) =>
+  fs.mkdirSync(path.join(mobilePicturesDirectory, directory))
+);
+pictures.forEach(({ ref, height, width }) => {
+  const fileExt = path.extname(ref);
+  const newFile = ref.replace(new RegExp(`${fileExt}$`), ".webp");
+  sharp(path.join(picturesDirectory, ref))
+    .resize(width, height)
+    .webp()
+    .toFile(path.join(mobilePicturesDirectory, newFile), (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+});
