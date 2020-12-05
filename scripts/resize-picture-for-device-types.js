325443bb2a590248b33eafe0f55718adf89d89d6
Roch DESCHASEAUX <roch.deschaseaux@mafassurances.onmicrosoft.com>
2020-12-05 12:40:14 +0100

    Trade uncessary variable for a chained method call

--- scripts/resize-picture-for-device-types.js
+++ scripts/resize-picture-for-device-types.js
@@ -13,42 +13,43 @@ const iPhoneRatio = deviceViewportHeight / deviceViewportWidth;
 const pictureDirectories = fs
   .readdirSync(picturesDirectory, { withFileTypes: true })
   .filter((dirent) => dirent.isDirectory());
-const pictures = pictureDirectories.flatMap(({ name: directory }) =>
-  fs.readdirSync(path.join(picturesDirectory, directory)).map((picture) => {
-    const ref = path.join(directory, picture);
-    const { height: pictureHeight, width: pictureWidth } = sizeOf(
-      path.join(picturesDirectory, ref)
-    );
-    const pictureRatio = pictureHeight / pictureWidth;
-    const isViewportMoreStretchedThanPicture = iPhoneRatio < pictureRatio;
-    const pictureHeightForDevice = isViewportMoreStretchedThanPicture
-      ? deviceViewportHeight
-      : (pictureHeight * deviceViewportWidth) / pictureWidth;
-    const pictureWidthForDevice = isViewportMoreStretchedThanPicture
-      ? (pictureWidth * deviceViewportHeight) / pictureHeight
-      : deviceViewportWidth;
-    return {
-      ref,
-      height: Math.round(pictureHeightForDevice),
-      width: Math.round(pictureWidthForDevice),
-    };
-  })
-);
 const deviceTypePicturesDirectory = `./pictures-by-device-type/${deviceType}`;
 fs.rmdirSync(deviceTypePicturesDirectory, { recursive: true });
 fs.mkdirSync(deviceTypePicturesDirectory);
 pictureDirectories.forEach(({ name: directory }) =>
   fs.mkdirSync(path.join(deviceTypePicturesDirectory, directory))
 );
-pictures.forEach(({ ref, height, width }) => {
-  const fileExt = path.extname(ref);
-  const newFile = ref.replace(new RegExp(`${fileExt}$`), ".webp");
-  sharp(path.join(picturesDirectory, ref))
-    .resize(width, height)
-    .webp()
-    .toFile(path.join(deviceTypePicturesDirectory, newFile), (err) => {
-      if (err) {
-        console.log(err);
-      }
-    });
-});
+pictureDirectories
+  .flatMap(({ name: directory }) =>
+    fs.readdirSync(path.join(picturesDirectory, directory)).map((picture) => {
+      const ref = path.join(directory, picture);
+      const { height: pictureHeight, width: pictureWidth } = sizeOf(
+        path.join(picturesDirectory, ref)
+      );
+      const pictureRatio = pictureHeight / pictureWidth;
+      const isViewportMoreStretchedThanPicture = iPhoneRatio < pictureRatio;
+      const pictureHeightForDevice = isViewportMoreStretchedThanPicture
+        ? deviceViewportHeight
+        : (pictureHeight * deviceViewportWidth) / pictureWidth;
+      const pictureWidthForDevice = isViewportMoreStretchedThanPicture
+        ? (pictureWidth * deviceViewportHeight) / pictureHeight
+        : deviceViewportWidth;
+      return {
+        ref,
+        height: Math.round(pictureHeightForDevice),
+        width: Math.round(pictureWidthForDevice),
+      };
+    })
+  )
+  .forEach(({ ref, height, width }) => {
+    const fileExt = path.extname(ref);
+    const newFile = ref.replace(new RegExp(`${fileExt}$`), ".webp");
+    sharp(path.join(picturesDirectory, ref))
+      .resize(width, height)
+      .webp()
+      .toFile(path.join(deviceTypePicturesDirectory, newFile), (err) => {
+        if (err) {
+          console.log(err);
+        }
+      });
+  });
