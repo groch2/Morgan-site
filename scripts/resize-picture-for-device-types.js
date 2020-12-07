bc8bc2b4c5c00cdb8b8b314f0850c74f6c31f4f0
Roch DESCHASEAUX <roch.deschaseaux@mafassurances.onmicrosoft.com>
2020-12-08 00:07:15 +0100

    Resize pictures for multiple devices viewport dimensions in one batch

--- scripts/resize-picture-for-device-types.js
+++ scripts/resize-picture-for-device-types.js
@@ -6,64 +6,73 @@ const sizeOf = require("image-size");
 const sharp = require("sharp");
 
 const picturesDirectory = "./pictures";
-const deviceType = "mobile";
 const factor = 1.5;
-const deviceViewportHeight = 428 * factor;
-const deviceViewportWidth = 926 * factor;
-const deviceViewportRatio = deviceViewportHeight / deviceViewportWidth;
-const pictureDirectories = fs
-  .readdirSync(picturesDirectory, { withFileTypes: true })
-  .filter((dirent) => dirent.isDirectory())
-  .map(({ name: directory }) => directory);
-const deviceTypePicturesDirectory = `./pictures-by-device-type/${deviceType}`;
-fs.rmdirSync(deviceTypePicturesDirectory, { recursive: true });
-fs.mkdirSync(deviceTypePicturesDirectory);
-const getTextWithoutLeadingNumber = (text) => /(?<=^\d+\s).+$/i.exec(text)[0];
-pictureDirectories.forEach((directory) =>
-  fs.mkdirSync(
-    path.join(
-      deviceTypePicturesDirectory,
-      getTextWithoutLeadingNumber(directory)
-    )
-  )
-);
-fs.writeFileSync(
-  path.join(deviceTypePicturesDirectory, "target-viewport-size.txt"),
-  `${Math.round(deviceViewportWidth)} x ${Math.round(deviceViewportHeight)}`
-);
-pictureDirectories
-  .flatMap((directory) =>
-    fs.readdirSync(path.join(picturesDirectory, directory)).map((picture) => {
-      const sourceRef = path.join(directory, picture);
-      const { height: pictureHeight, width: pictureWidth } = sizeOf(
-        path.join(picturesDirectory, sourceRef)
-      );
-      const pictureRatio = pictureHeight / pictureWidth;
-      const isViewportMoreStretchedThanPicture =
-        deviceViewportRatio < pictureRatio;
-      const pictureHeightForDevice = isViewportMoreStretchedThanPicture
-        ? deviceViewportHeight
-        : (pictureHeight * deviceViewportWidth) / pictureWidth;
-      const pictureWidthForDevice = isViewportMoreStretchedThanPicture
-        ? (pictureWidth * deviceViewportHeight) / pictureHeight
-        : deviceViewportWidth;
-      return {
-        sourceRef,
-        targetRef: path.join(getTextWithoutLeadingNumber(directory), picture),
-        height: Math.round(pictureHeightForDevice),
-        width: Math.round(pictureWidthForDevice),
-      };
-    })
-  )
-  .forEach(({ sourceRef, targetRef, height, width }) => {
-    const fileExt = path.extname(targetRef);
-    const newFile = targetRef.replace(new RegExp(`${fileExt}$`), ".webp");
-    sharp(path.join(picturesDirectory, sourceRef))
-      .resize(width, height)
-      .webp()
-      .toFile(path.join(deviceTypePicturesDirectory, newFile), (err) => {
-        if (err) {
-          console.log(err);
-        }
+require("./viewport-dimensions-by-device.json").forEach(
+  ({ device, width, height }) => {
+    const [originalWidth, originalHeight] = [width, height];
+    [width, height] = [width, height].map((n) => parseInt(n) * factor);
+    const deviceViewportRatio = height / width;
+    const pictureDirectories = fs
+      .readdirSync(picturesDirectory, { withFileTypes: true })
+      .filter((dirent) => dirent.isDirectory())
+      .map(({ name: directory }) => directory);
+    const deviceTypePicturesDirectory = `./pictures-by-device-type/${device}`;
+    fs.rmdirSync(deviceTypePicturesDirectory, { recursive: true });
+    fs.mkdirSync(deviceTypePicturesDirectory);
+    fs.writeFileSync(
+      path.join(deviceTypePicturesDirectory, "target-viewport-size.txt"),
+      `${originalWidth} x ${originalHeight}`
+    );
+    const getTextWithoutLeadingNumber = (text) =>
+      /(?<=^\d+\s).+$/i.exec(text)[0];
+    pictureDirectories.forEach((directory) =>
+      fs.mkdirSync(
+        path.join(
+          deviceTypePicturesDirectory,
+          getTextWithoutLeadingNumber(directory)
+        )
+      )
+    );
+    pictureDirectories
+      .flatMap((directory) =>
+        fs
+          .readdirSync(path.join(picturesDirectory, directory))
+          .map((picture) => {
+            const sourceRef = path.join(directory, picture);
+            const { height: pictureHeight, width: pictureWidth } = sizeOf(
+              path.join(picturesDirectory, sourceRef)
+            );
+            const pictureRatio = pictureHeight / pictureWidth;
+            const isViewportMoreStretchedThanPicture =
+              deviceViewportRatio < pictureRatio;
+            const pictureHeightForDevice = isViewportMoreStretchedThanPicture
+              ? height
+              : (pictureHeight * width) / pictureWidth;
+            const pictureWidthForDevice = isViewportMoreStretchedThanPicture
+              ? (pictureWidth * height) / pictureHeight
+              : width;
+            return {
+              sourceRef,
+              targetRef: path.join(
+                getTextWithoutLeadingNumber(directory),
+                picture
+              ),
+              height: Math.round(pictureHeightForDevice),
+              width: Math.round(pictureWidthForDevice),
+            };
+          })
+      )
+      .forEach(({ sourceRef, targetRef, height, width }) => {
+        const fileExt = path.extname(targetRef);
+        const newFile = targetRef.replace(new RegExp(`${fileExt}$`), ".webp");
+        sharp(path.join(picturesDirectory, sourceRef))
+          .resize(width, height)
+          .webp()
+          .toFile(path.join(deviceTypePicturesDirectory, newFile), (err) => {
+            if (err) {
+              console.log(err);
+            }
+          });
       });
-  });
+  }
+);
