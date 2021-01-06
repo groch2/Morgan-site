72ccee12ddd22afb18ecda2001ad5b015e8c1191
Roch DESCHASEAUX <roch.deschaseaux@mafassurances.onmicrosoft.com>
2021-01-06 23:32:22 +0100

    opening the side nav pushes the main part to the right

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const sideNav = document.getElementById("home-sidenav");
+const main = document.getElementsByTagName("main")[0];
 document.getElementById("burger-menu").addEventListener("click", function () {
   sideNav.style.width = "250px";
+  main.style.marginLeft = "250px";
+  main.style.marginRight = "-250px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 });
 sideNav
   .querySelectorAll("a[class='closebtn']")[0]
   .addEventListener("click", function () {
     sideNav.style.width = "0";
+    main.style.marginLeft = "0";
+    main.style.marginRight = "0px";
+    document.body.style.backgroundColor = "white";
   });
