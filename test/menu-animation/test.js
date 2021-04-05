49ce7837101f51786422aaa53e01c55a7f4b68d6
Roch DESCHASEAUX <roch.deschaseaux@mafassurances.onmicrosoft.com>
2021-04-05 16:21:18 +0200

    working POC for header auto hidding by Y scrolling

--- test/menu-animation/test.js
+++ test/menu-animation/test.js
@@ -6,13 +6,33 @@
   } = window.getComputedStyle(header);
   const getSizeValue = (sizeWithUnit) =>
     parseFloat(/^\d+\.?\d*/.exec(sizeWithUnit)[0]);
-  const totalHeight =
+  const totalHeaderHeight =
     header.offsetHeight +
     getSizeValue(headerMarginTop) +
     getSizeValue(headerMarginBottom);
-  let isOpen = true;
-  document.getElementById("btnTest").addEventListener("click", function () {
-    header.style.top = isOpen ? `-${totalHeight}px` : 0;
-    isOpen = !isOpen;
+  document.getElementsByTagName(
+    "p"
+  )[0].style.paddingTop = `${totalHeaderHeight}px`;
+
+  const distanceForHeaderSwitching = totalHeaderHeight * 0.75;
+  let lastKnownScrollPosition = 0;
+  let currentDirection = "DOWN";
+  let positionAtLastScrollDirectionChange = 0;
+  document.addEventListener("scroll", function () {
+    const newDirection =
+      lastKnownScrollPosition > window.scrollY ? "UP" : "DOWN";
+    if (currentDirection != newDirection) {
+      positionAtLastScrollDirectionChange = window.scrollY;
+      currentDirection = newDirection;
+    } else {
+      const diffFromPreviouDirectionChangePosition = Math.abs(
+        window.scrollY - positionAtLastScrollDirectionChange
+      );
+      if (diffFromPreviouDirectionChangePosition > distanceForHeaderSwitching) {
+        header.style.top =
+          newDirection == "DOWN" ? `-${totalHeaderHeight}px` : 0;
+      }
+    }
+    lastKnownScrollPosition = window.scrollY;
   });
 })();
