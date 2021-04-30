5f6bd3d1e1caa056320c7031c91920bda130109b
Roch DESCHASEAUX <roch.deschaseaux@mafassurances.onmicrosoft.com>
2021-04-30 07:16:56 +0200

    make the deployment script display the dry run mode output before deploying for good

--- scripts/deploy-to-aws-s3.ps1
+++ scripts/deploy-to-aws-s3.ps1
@@ -1,1 +1,9 @@
 aws s3 sync "..\dist" s3://morgan-site-test/ --include "*" --exclude "pictures-by-device-type/*" --delete --dryrun
+Write-Host "Press Enter to confirm or any other key to abort :"
+$keyCode = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown").VirtualKeyCode
+if ($keyCode -eq 13) {
+    aws s3 sync "..\dist" s3://morgan-site-test/ --include "*" --exclude "pictures-by-device-type/*" --delete
+}
+else {
+    Write-Host "Abort"
+}
