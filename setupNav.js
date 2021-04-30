0afb79ce38f1693e403299f78382f89653bf3cf4
Roch DESCHASEAUX <roch.deschaseaux@mafassurances.onmicrosoft.com>
2021-04-30 07:51:25 +0200

    remove console log

--- setupNav.js
+++ setupNav.js
@@ -31,7 +31,6 @@ export function setupNav(onNavChange) {
     let navOpen = false;
     [burgerMenu, main].forEach((item) =>
       item.addEventListener("click", function ({ target }) {
-        console.log("click on ", target.tagName);
         const isBurgerMenuClick = burgerMenu.contains(target);
         if (!navOpen && !isBurgerMenuClick) {
           return;
