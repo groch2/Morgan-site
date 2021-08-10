2664256ee54cd6465ad459993abfcd5946a42932
Roch DESCHASEAUX <roch.deschaseaux@mafassurances.onmicrosoft.com>
2021-08-10 15:58:36 +0200

    fix the display of the site title when the left side side navigation menu is visible on the home page

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
 import { setupNav } from "./setupNav";
 
-const onNavChange = (function () {
-  const headerTitle = document.querySelector("header > h1");
-  let _isNavOpen = false;
-  return () => {
-    headerTitle.style.opacity = _isNavOpen ? "1" : "0";
-    _isNavOpen = !_isNavOpen;
-  };
-})();
-
-setupNav(onNavChange);
+setupNav();
