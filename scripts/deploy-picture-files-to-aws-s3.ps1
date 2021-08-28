088fbe1695058ec22b4f880f67d81cf66a573eb9
Roch DESCHASEAUX <roch.deschaseaux@mafassurances.onmicrosoft.com>
2021-08-28 16:13:08 +0200

    add a script to synchronize the pictures directory with the Amazon s3 bucket directory use a variable for the base aws s3 sync command in the deployment scripts

---
+++ scripts/deploy-picture-files-to-aws-s3.ps1
@@ -0,0 +1,10 @@
+$deployCommand = "aws s3 sync "".\pictures-by-device-type"" s3://morgan-site-test/pictures-by-device-type/ --delete"
+Invoke-Expression "${deployCommand} --dryrun"
+Write-Host "Press Enter to confirm or any other key to abort :"
+$keyCode = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown").VirtualKeyCode
+if ($keyCode -eq 13) {
+    Invoke-Expression "${deployCommand}"
+}
+else {
+    Write-Host "Abort"
+}
---
+++ scripts/deploy-text-files-to-aws-s3.ps1
@@ -0,0 +1,10 @@
+$deployCommand = "aws s3 sync "".\dist"" s3://morgan-site-test/ --include ""*"" --exclude ""pictures-by-device-type/*"" --delete"
+Invoke-Expression "${deployCommand} --dryrun"
+Write-Host "Press Enter to confirm or any other key to abort :"
+$keyCode = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown").VirtualKeyCode
+if ($keyCode -eq 13) {
+    Invoke-Expression "${deployCommand}"
+}
+else {
+    Write-Host "Abort"
+}
