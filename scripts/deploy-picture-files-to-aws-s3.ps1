f2641200aaf743e16dea0e07713bfe41e199af66
Roch DESCHASEAUX <roch.deschaseaux@mafassurances.onmicrosoft.com>
2021-11-20 11:13:48 +0100

    deploy the pictures to a distinct aws bucket from the one used for the other parts of the static site

--- scripts/deploy-picture-files-to-aws-s3.ps1
+++ scripts/deploy-picture-files-to-aws-s3.ps1
@@ -1,4 +1,4 @@
-$deployCommand = "aws s3 sync "".\pictures-by-device-type"" s3://morgan-site-test/pictures-by-device-type/ --delete"
+$deployCommand = "aws s3 sync "".\pictures-by-device-type"" s3://morgan-site-pictures-by-device-type/ --delete"
 Invoke-Expression "${deployCommand} --dryrun"
 Write-Host "Press Enter to confirm or any other key to abort :"
 $keyCode = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown").VirtualKeyCode
